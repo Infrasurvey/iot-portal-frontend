97f445a7f583cf096fc3c011fade4680abf47c74
Jeannet Nino <nino.jeannet@einet.ad.eivd.ch>
2021-06-11 15:01:28 +0200

    add validation for pwd recovery

--- src/components/auth/Recovery.vue
+++ src/components/auth/Recovery.vue
@@ -4,9 +4,11 @@
     <form action="#" @submit.prevent="sendMail">
 
       <!-- Email entry -->
-      <md-field>
+      <md-field :class="getValidationClass('mail')">
         <label>E-mail</label>
         <md-input type="email" v-model="mail"></md-input>
+        <span class="md-error" v-if="!$v.mail.required">Please enter an email</span>
+        <span class="md-error" v-if="!$v.mail.email">Please enter a valid email</span>
       </md-field>
 
       <!--  Send email button -->
@@ -18,6 +20,7 @@
 
 <script>
 import API from '../../http-constants'
+import { required,email} from 'vuelidate/lib/validators'
 
 export default {
   name: 'login',
@@ -26,27 +29,44 @@ export default {
       mail: ''
     }
   },
+  validations:{
+    mail:{
+      required,
+      email
+    }
+  },
   methods: {
       
     sendMail() {
+      this.$v.$touch();
+      if(this.$v.$error) return
       API.post('/api/resetPassword',{'email':this.mail})
-            .then(response => {
-                this.displayStatus(true)
-            })
-            .catch(e => {
-                this.displayStatus(false)
-            })
+      .then(response => {
+          this.displayStatus(true)
+      })
+      .catch(e => {
+          this.displayStatus(false)
+      })
         
     },
-    displayStatus(status){
-            if(status){
-                this.flashMessage.success({title: 'Success', message: 'An email has been sent to your email with a new password.'});
-            }
-            else
-            {
-                this.flashMessage.show({status: 'error', title: 'Error', message: 'An error occured during password reset'})
-            }
+    getValidationClass (fieldName) {
+      const field = this.$v[fieldName]
+
+      if (field) {
+        return {
+          'md-invalid': field.$invalid && field.$dirty
         }
+      }
+    },
+    displayStatus(status){
+        if(status){
+            this.flashMessage.success({title: 'Success', message: 'An email has been sent to your email with a new password.'});
+        }
+        else
+        {
+            this.flashMessage.show({status: 'error', title: 'Error', message: 'An error occured during password reset'})
+        }
+    }
   }
 }
 </script>
